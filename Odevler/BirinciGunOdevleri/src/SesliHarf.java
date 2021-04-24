41e2bcb232e32e5ea342021698231cef1f7ab979
aslihan98 <70073898+aslihan98@users.noreply.github.com>
2021-04-24 15:37:50 +0300

    1.gün ödevleri eklendi.

---
+++ Odevler/BirinciGunOdevleri/src/SesliHarf.java
@@ -0,0 +1,20 @@
+
+public class SesliHarf {
+
+	public static void main(String[] args) {
+		char harf = 'E';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'U':
+		case 'O':
+			System.out.println("Sesli harftir.");
+			break;
+		default:
+			System.out.println("Sessiz harftir.");
+		}
+
+	}
+
+}
